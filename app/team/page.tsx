b7b3e7b27d55d5642941047e88ca65b5ae104ed8
Rehan Perera <pererarehan2007@gmail.com>
2026-01-01 22:06:30 +0530

    refactor: add `as const` type assertion to animation type property

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -33,7 +33,7 @@ export default function TeamPage() {
             opacity: 1,
             y: 0,
             transition: {
-                type: "spring",
+                type: "spring" as const,
                 bounce: 0.3,
                 duration: 0.8
             }
